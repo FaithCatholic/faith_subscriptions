bc4d97cc2737091f74ef4a44dea1d06c28c74c4a
Nicholas Ketchum <nketchum@gmail.com>
2023-07-07 17:32:17 +0000

    remove project from info file to avoid warning on reports

---
+++ js/scripts.es6.js
@@ -0,0 +1,9 @@
+(function ($, Drupal, once) {
+
+  function myFunction() {
+    console.log('Faith Subscriptions theme scripts is alive and well.');
+  };
+
+  myFunction();
+
+})(jQuery, Drupal, once);
